4d7c09f42fc11e4e40a8cbbcc3baee5ad8c7c349
aditrich <richardwilson@aditdigital.com>
2022-07-10 22:46:38 -0500

    fixing xsrf issue

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -19,7 +19,7 @@ const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
-        'X-XSRF-TOKEN': xsrf,
+        'X-XSRF-TOKEN': xsrf ? xsrf : '',
     },
     withCredentials: true,
 })
