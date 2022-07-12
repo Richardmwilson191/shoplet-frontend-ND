733d8f60853e9a02a34b9669c63555e88dd8271e
Richard Wilson <richardmwilson191@gmail.com>
2022-07-11 20:30:34 -0500

    Fixed mobile menu links

--- src/components/Layouts/Navigation.js
+++ src/components/Layouts/Navigation.js
@@ -10,11 +10,13 @@ import {
     MenuIcon,
     ShoppingBagIcon,
     XIcon,
+    LoginIcon,
 } from '@heroicons/react/outline'
 import { MdOutlineSell } from 'react-icons/md'
 
 import { useAuth } from '@/hooks/auth'
 import SearchBox from '@/components/SearchBox'
+import { HiOutlineDocumentText } from 'react-icons/hi'
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -232,80 +234,72 @@ export default function Navigation() {
                         <Disclosure.Panel className="lg:hidden">
                             <div className="pt-2 pb-3 space-y-1">
                                 {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" */}
-                                <Disclosure.Button
-                                    as="a"
-                                    href="#"
-                                    className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
-                                    Dashboard
-                                </Disclosure.Button>
-                                <Disclosure.Button
-                                    as="a"
-                                    href="#"
-                                    className="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
-                                    Team
-                                </Disclosure.Button>
-                                <Disclosure.Button
-                                    as="a"
-                                    href="#"
-                                    className="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
-                                    Projects
-                                </Disclosure.Button>
-                                <Disclosure.Button
-                                    as="a"
-                                    href="#"
-                                    className="border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
-                                    Calendar
-                                </Disclosure.Button>
-                            </div>
-                            <div className="pt-4 pb-3 border-t border-gray-200">
-                                <div className="flex items-center px-4">
-                                    <div className="flex-shrink-0">
-                                        <img
-                                            className="h-10 w-10 rounded-full"
-                                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                            alt=""
-                                        />
+                                {menu.map(item => (
+                                    <div key={item.name} className="flex">
+                                        <Link href={item.href}>
+                                            <a
+                                                className={`${
+                                                    item.href ===
+                                                    router.pathname
+                                                        ? 'bg-indigo-200 text-indigo-600'
+                                                        : 'border-transparent'
+                                                } text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-2 pb-2 border-b-2 text-md font-medium w-full`}>
+                                                <item.icon
+                                                    className="mr-1 flex-shrink-0 h-6 w-6 text-gray-500"
+                                                    aria-hidden="true"
+                                                />
+                                                {item.name}
+                                            </a>
+                                        </Link>
                                     </div>
-                                    <div className="ml-3">
-                                        <div className="text-base font-medium text-gray-800">
-                                            Tom Cook
+                                ))}
+                                {!user ? (
+                                    <>
+                                        <div className="flex">
+                                            <Link href="/login">
+                                                <a
+                                                    className={`${
+                                                        '/login' ===
+                                                        router.pathname
+                                                            ? 'bg-indigo-200 text-indigo-600'
+                                                            : 'border-transparent'
+                                                    } text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-2 pb-2 border-b-2 text-md font-medium w-full`}>
+                                                    <LoginIcon
+                                                        className="mr-1 flex-shrink-0 h-6 w-6 text-gray-500"
+                                                        aria-hidden="true"
+                                                    />
+                                                    Login
+                                                </a>
+                                            </Link>
                                         </div>
-                                        <div className="text-sm font-medium text-gray-500">
-                                            tom@example.com
+
+                                        <div className="flex">
+                                            <Link href="/register">
+                                                <a
+                                                    className={`${
+                                                        '/register' ===
+                                                        router.pathname
+                                                            ? 'bg-indigo-200 text-indigo-600'
+                                                            : 'border-transparent'
+                                                    } text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-2 pb-2 border-b-2 text-md font-medium w-full`}>
+                                                    <HiOutlineDocumentText
+                                                        className="mr-1 flex-shrink-0 h-6 w-6 text-gray-500"
+                                                        aria-hidden="true"
+                                                    />
+                                                    Register
+                                                </a>
+                                            </Link>
                                         </div>
-                                    </div>
-                                    <button
-                                        type="button"
-                                        className="ml-auto flex-shrink-0 bg-white p-1 text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                        <span className="sr-only">
-                                            View notifications
-                                        </span>
-                                        <BellIcon
-                                            className="h-6 w-6"
-                                            aria-hidden="true"
-                                        />
-                                    </button>
-                                </div>
-                                <div className="mt-3 space-y-1">
-                                    <Disclosure.Button
-                                        as="a"
-                                        href="#"
-                                        className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">
-                                        Your Profile
-                                    </Disclosure.Button>
-                                    <Disclosure.Button
-                                        as="a"
-                                        href="#"
-                                        className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">
-                                        Settings
-                                    </Disclosure.Button>
-                                    <Disclosure.Button
-                                        as="a"
-                                        href="#"
-                                        className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">
-                                        Sign out
-                                    </Disclosure.Button>
-                                </div>
+                                    </>
+                                ) : (
+                                    <>
+                                        <button
+                                            onClick={logout}
+                                            className="hover:bg-indigo-50 hover:border-indigo-500 hover:text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
+                                            Sign out
+                                        </button>
+                                    </>
+                                )}
                             </div>
                         </Disclosure.Panel>
                     </>
